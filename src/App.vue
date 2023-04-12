717d937a21d73b1399c842f74a92390426088a56
871850262 <871850262@qq.com>
2023-04-12 22:12:32 +0800

    initiate project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <van-button type="primary" >点击</van-button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import 'vant/lib/index.css'
+import './style/main.less'
+import App from './App.vue'
+
+import { } from 'vant'
+createApp(App).mount('#app')
